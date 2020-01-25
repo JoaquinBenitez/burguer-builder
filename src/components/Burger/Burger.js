214e3b9ce165253ea04fd52edc559a7acfc5ed51
Joaquin <joabenitez@gmail.com>
2020-01-25 16:18:29 -0300

    re writed the code to have a better understanding

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,27 @@ import styles from "./Burger.module.css";
 
 const Burger = props => {
   //need to convert Obj to Arr to use map
+  /*
   const arrayOfIngredients = Object.keys(props.ingredients).map(ingKey => {
     return [...Array(props.ingredients[ingKey])].map((_, i) => {
-      
       return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+    });
+  });
+  */
+  //doing the logic on my own instead of just copying:
+  const arrayIngredients = Object.keys(props.ingredients).map(key => {
+    // console.log(arrayIngredients[key]); arIng["salad"], arIng["meat"] etc -> undefined
+    return [...Array(props.ingredients[key])].map((k, i) => {
+      //console.log([...Array(props.ingredients[key])]); this returns [undefined] or [undef, undef] or more
+      //console.log(k, i); key is undefined and i is the index, i is useful for the key of the list
+      return <BurgerIngredient key={key + i} type={key} />;
     });
   });
 
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      {arrayOfIngredients}
+      {arrayIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
