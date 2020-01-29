e0e4be086e6f6821255be0383f994ba78ca4435b
Joaquin <joabenitez@gmail.com>
2020-01-29 09:28:34 -0300

    Using memo for perfomance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import CustomButton from "../../UI/Button/CustomButton";
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(key => {
-    return (
-      <li key={key}>
-        <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
-        {props.ingredients[key]}
-      </li>
+class OrderSummary extends Component {  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      key => {
+        return (
+          <li key={key}>
+            <span style={{ textTransform: "capitalize" }}>{key}</span>:{" "}
+            {this.props.ingredients[key]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <>
-      <h4>Your order:</h4>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        Total price: <strong>{props.totalPrice.toFixed(2)}</strong>{" "}
-      </p>
-      <p>Continue to checkout?</p>
-      <CustomButton btnType="Cancel" clicked={props.clickCancel}>
-        {" "}
-        CANCEL{" "}
-      </CustomButton>
-      <CustomButton btnType="Continue" clicked={props.clickContinue}>
-        {" "}
-        CONTINUE{" "}
-      </CustomButton>
-    </>
-  );
-};
+
+    return (
+      <>
+        <h4>Your order:</h4>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          Total price: <strong>{this.props.totalPrice.toFixed(2)}</strong>{" "}
+        </p>
+        <p>Continue to checkout?</p>
+        <CustomButton btnType="Cancel" clicked={this.props.clickCancel}>
+          {" "}
+          CANCEL{" "}
+        </CustomButton>
+        <CustomButton btnType="Continue" clicked={this.props.clickContinue}>
+          {" "}
+          CONTINUE{" "}
+        </CustomButton>
+      </>
+    );
+  }
+}
 
 export default OrderSummary;
