c2bde0002ec94278d76e1902d7b8db8baf01d448
Joaquin <joabenitez@gmail.com>
2020-02-04 18:23:00 -0300

    managing errors with axios and higher order comp

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders.js";
 import LoadingSpinner from "../../components/UI/LoadingSpinner/LoadingSpinner";
+import WithErrorHandler from "../../components/hoc/WithErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.3,
@@ -146,4 +147,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default WithErrorHandler(BurgerBuilder, axios);
