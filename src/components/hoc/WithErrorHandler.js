8657d41749e3cf019159f9970eb8e86276706189
Joaquin <joabenitez@gmail.com>
2020-02-04 18:50:01 -0300

    unmounting axios interceptors

--- src/components/hoc/WithErrorHandler.js
+++ src/components/hoc/WithErrorHandler.js
@@ -6,17 +6,24 @@ const WithErrorHandler = (WrappedComponent, axios) => {
     state = {
       error: null
     };
+
     componentDidMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      //this is to prevent memory leaking
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorMessageHandler = () => {
